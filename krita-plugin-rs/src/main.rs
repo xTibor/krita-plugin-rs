041b723b2c9950795d3baf0a7d6e21c6cc1b4f35
Tibor Nagy <xnagytibor@gmail.com>
2020-04-15 18:10:16 +0200

    Wrap the data into an ImageBuffer from the image crate

--- krita-plugin-rs/src/main.rs
+++ krita-plugin-rs/src/main.rs
@@ -1,27 +1,27 @@
 use std::io::{self, Write, Read};
 use std::env;
+use image::{ImageBuffer, Bgra};
+
+// The image crate doesn't export BGRA image types for some reason.
+type BgraImage = ImageBuffer<Bgra<u8>, Vec<u8>>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let width: u32 = args[1].parse().unwrap();
+    let height: u32 = args[2].parse().unwrap();
 
-    let width: usize = args[1].parse().unwrap();
-    let height: usize = args[2].parse().unwrap();
-
-    let data_size = width * height * 4;
+    let data_size = (width * height * 4) as usize;
     let mut data: Vec<u8> = vec![0; data_size];
     io::stdin().read_exact(&mut data).unwrap();
 
+    let mut image_buffer = BgraImage::from_raw(width, height, data).unwrap();
+
     for y in 0..height {
         for x in 0..width {
-            let i = (y * width + x) * 4;
-            data[i + 0] = (x & y) as u8; // B
-            data[i + 1] = (x | y) as u8; // G
-            data[i + 2] = (x ^ y) as u8; // R
-
-            // Keep alpha for demo
-            //data[i + 3] = 0xFF; // A
+            let k = (x ^ y) as u8;
+            image_buffer.put_pixel(x, y, Bgra([255, k, 255 - k, k]));
         }
     }
 
-    io::stdout().write_all(&data).unwrap();
+    io::stdout().write_all(&image_buffer.into_raw()).unwrap();
 }
