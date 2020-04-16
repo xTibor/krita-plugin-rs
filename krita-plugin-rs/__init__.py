d51eca380502338e1a5c3332bdb54778dcf20cfd
Tibor Nagy <xnagytibor@gmail.com>
2020-04-16 16:53:33 +0200

    Perform action on selections in place

--- krita-plugin-rs/__init__.py
+++ krita-plugin-rs/__init__.py
@@ -20,28 +20,44 @@ class RustExtension(Extension):
 
     def executePlugin(self):
         doc = Krita.instance().activeDocument()
-        if doc is not None:
-            manifestPath = os.path.dirname(__file__) + '/Cargo.toml'
-            process = subprocess.Popen(
-                [
-                    'cargo', 'run',
-                    '--release',
-                    '--manifest-path', manifestPath,
-                    '--',
-                    str(doc.width()),
-                    str(doc.height())
-                ],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                cwd=os.path.dirname(__file__)
-            )
-            process.stdin.write(doc.pixelData(0, 0, doc.width(), doc.height()))
+        if doc is None:
+            return
 
-            resultPixelData = process.communicate()[0]
+        selection = doc.selection()
+        if selection is not None:
+            width, height = selection.width(), selection.height()
+            sourcePixelData = doc.activeNode().pixelData(selection.x(), selection.y(), selection.width(), selection.height())
+        else:
+            width, height = doc.width(), doc.height()
+            sourcePixelData = doc.pixelData(0, 0, doc.width(), doc.height())
 
+        manifestPath = os.path.dirname(__file__) + '/Cargo.toml'
+        process = subprocess.Popen(
+            [
+                'cargo', 'run',
+                '--release',
+                '--manifest-path', manifestPath,
+                '--',
+                str(width),
+                str(height)
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            cwd=os.path.dirname(__file__)
+        )
+        process.stdin.write(sourcePixelData)
+
+        resultPixelData = process.communicate()[0]
+        if process.returncode != 0:
+            return
+
+        if selection is not None:
+            doc.activeNode().setPixelData(resultPixelData, selection.x(), selection.y(), selection.width(), selection.height())
+        else:
             newLayer = doc.createNode("rustLayer", "paintLayer")
             newLayer.setPixelData(resultPixelData, 0, 0, doc.width(), doc.height())
             doc.rootNode().addChildNode(newLayer, None)
-            doc.refreshProjection()
+
+        doc.refreshProjection()
 
 Krita.instance().addExtension(RustExtension(Krita.instance()))
